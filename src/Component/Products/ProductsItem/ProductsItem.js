83ac98069d89831c904fdf047fe45507b1312f3a
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-08-08 22:01:11 -0700

    update

--- src/Component/Products/ProductsItem/ProductsItem.js
+++ src/Component/Products/ProductsItem/ProductsItem.js
@@ -9,12 +9,13 @@ import ElectricFreezer from "./ProductItems/Electric_Freezer";
 const ProductsItem = (props) => {
     return (
         <div>
+            <h1>Products</h1>
             <ul className={classes.ProductsList}>
-                <li><a href="/"><ElectricCooler />ElectricCooler</a></li>
-                <li><a href="/"><ElectricFan />ElectricFan</a></li>
-                <li><a href="/"><ElectricFreezer />ElectricFreezer</a></li>
-                <li><a href="/"><ElectricHeater />ElectricHeater</a></li>
-                <li><a href="/"><ElectricIron />ElectricIron</a></li>
+                <li><ElectricCooler /></li>
+                <li><ElectricFan /></li>
+                <li><ElectricFreezer /></li>
+                <li><ElectricHeater /></li>
+                <li><ElectricIron /></li>
 
             </ul>
         </div>
