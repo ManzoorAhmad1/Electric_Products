c3962a656b45712a4af4a5c963d95e4314150c8f
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-08-10 09:05:04 -0700

    use Link for routing

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import classes from './Navbar.module.css';
+import { Link } from "react-router-dom";
 const Navbar = (props) => {
     return (
         <div className={classes.Navbar}>
             <div className={classes.NavbarContent}>
                 <h2>M-A Electronics</h2>
                 <ul className={classes.NavbarList}>
-                    <li><a href="/">home</a></li>
-                    <li><a href="about">about</a></li>
+                    <li><Link to="/">home</Link></li>
+                    <li><Link to="about">about</Link></li>
                 </ul>
             </div>
 
