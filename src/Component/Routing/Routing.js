3ddfc5dc9b74b9e06c2beeeb1a862538740a6e19
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-08-10 22:52:00 -0700

    add logic for login form

--- src/Component/Routing/Routing.js
+++ src/Component/Routing/Routing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../Navbar/Navbar";
+import { useState } from "react";
 import GFCCooler from "../ProductsDetail/Cooler/GFC_Cooler"
 import RoyleCooler from "../ProductsDetail/Cooler/Royle_Cooler"
 import AsiaCooler from "../ProductsDetail/Cooler/Asia_Cooler"
@@ -10,35 +10,47 @@ import RoyleFan from "../ProductsDetail/Fan/Royle_Fan"
 import LahoreFan from "../ProductsDetail/Fan/Lahore_Fan"
 import DawlanceFreezer from "../ProductsDetail/Freezer/Dawlance_Freezer"
 import HaierFreezer from "../ProductsDetail/Freezer/Haier_Freezer"
+import Navbar from '../Navbar/Navbar';
 import PelFreezer from "../ProductsDetail/Freezer/Pel_Freezer"
 import Home from "../Home/Home"
 import About from "../About/About"
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import LoginForm from "../LoginForm/LoginForm";
 const Routing = (props) => {
+    const [loginFrom, setLoginForm] = useState(false);
+    const onClickHandler = () => {
+        setLoginForm(true)
+    }
+
     return (
-        <BrowserRouter>
-            <Navbar />
-            <div className={classes.products}>
-                <div className={classes.ProductsItem}>
-                    <ProductsItem />
-                </div>
-                <div className={classes.ProductsDetails}>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/GFCCooler" element={<GFCCooler />} />
-                        <Route path="/RoyleCooler" element={<RoyleCooler />} />
-                        <Route path="/AsiaCooler" element={<AsiaCooler />} />
-                        <Route path="/GFCFan" element={<GFCFan />} />
-                        <Route path="/RoyleFan" element={<RoyleFan />} />
-                        <Route path="/LahoreFan" element={<LahoreFan />} />
-                        <Route path="/DawlanceFreezer" element={<DawlanceFreezer />} />
-                        <Route path="/HaierFreezer" element={<HaierFreezer />} />
-                        <Route path="/PelFreezer" element={<PelFreezer />} />
-                    </Routes>
-                </div>
-            </div>
-        </BrowserRouter>
+        <div>
+            {loginFrom ? <LoginForm loginForm={setLoginForm} /> :
+                <BrowserRouter>
+                    <Navbar onClick={onClickHandler} />
+                    <div className={classes.products}>
+                        <div className={classes.ProductsItem}>
+                            <ProductsItem />
+                        </div>
+                        <div className={classes.ProductsDetails}>
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/about" element={<About />} />
+                                <Route path="/GFCCooler" element={<GFCCooler />} />
+                                <Route path="/RoyleCooler" element={<RoyleCooler />} />
+                                <Route path="/AsiaCooler" element={<AsiaCooler />} />
+                                <Route path="/GFCFan" element={<GFCFan />} />
+                                <Route path="/RoyleFan" element={<RoyleFan />} />
+                                <Route path="/LahoreFan" element={<LahoreFan />} />
+                                <Route path="/DawlanceFreezer" element={<DawlanceFreezer />} />
+                                <Route path="/HaierFreezer" element={<HaierFreezer />} />
+                                <Route path="/PelFreezer" element={<PelFreezer />} />
+                            </Routes>
+                        </div>
+                    </div>
+                </BrowserRouter>}
+        </div>
+
     )
 }
 export default Routing;
