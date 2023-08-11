584d8c1d0d0aabf80581fb0ed5be833fa49aabb6
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-08-10 22:52:28 -0700

    create Loading  bar and styles

---
+++ src/Component/UI/LoadingBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./LoadingBar.module.css";
+const LoadingBar = (props) => {
+    return (
+        <div className={classes.loadingBar} >
+
+        </div>
+    )
+}
+export default LoadingBar;
